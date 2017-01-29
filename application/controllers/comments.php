5d4c290c78d07209261544fbad6e7eaaff4c4a3f
dnizetic <denis_nizetic@hotmail.com>
2017-01-29 17:13:45 +0100

    Pass proper data to comments view.

--- application/controllers/comments.php
+++ application/controllers/comments.php
@@ -5,16 +5,36 @@ if (!defined('BASEPATH'))
 
 class Comments extends MY_Controller {
 
-   
     public function show($item_id) {
-        
+
         $this->load->model('comments_model');
+
         
-        $data['comments'] = $this->comments_model->get_many_by(array(
+        /**
+         * Sort comments by date before fetching them.
+         */
+        $data['comments'] = $this->comments_model->order_by('date')->get_many_by(array(
             'item_id' => $item_id
         ));
+
         
-        $this->load->view('comments/show', $data);
+        /**
+         * Long polling needs to pass item id in order
+         * to check for new rows.
+         */
+        $data['item_id'] = $item_id;
+        
+        
+        /**
+         * Get date field from last row. With this date we'll check for newly added rows.
+         * If a row in DB has a greater add date than this, then we'll know
+         * it's a newly added row.
+         */
+        $last_row_index = count($data['comments']) - 1;
+        $data['latest_date'] = $data['comments'][$last_row_index]->date;
+        
+
+        $this->call_template('comments/show', $data);
     }
 
 }
