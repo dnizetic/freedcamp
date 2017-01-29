4dc46e7713c74cbd672f1231922465a4ed18ecce
dnizetic <denis_nizetic@hotmail.com>
2017-01-29 15:13:40 +0100

    Add Show comment route. Add comments controller.

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,6 +40,7 @@
 
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
+$route['comments/(:num)'] = 'comments/show/$1';
 
 
 /* End of file routes.php */
---
+++ application/controllers/comments.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Comments extends CI_Controller {
+
+   
+    public function show($id) {
+        
+        
+        
+    }
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
