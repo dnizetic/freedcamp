6eca47fc29f36ed48d83dc9b444a88c885638afc
dnizetic <denis_nizetic@hotmail.com>
2017-01-29 17:21:48 +0100

    Install bootstrap CSS. Update readme.

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -1,6 +1,3 @@
-<?php
-$template_path = $this->template_path;
-?>
 <!DOCTYPE html>
 <html>
 
@@ -13,7 +10,11 @@ $template_path = $this->template_path;
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <!-- Basic Styles -->
-        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $template_path; ?>css/bootstrap.min.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
         <!-- Table styles. -->
         <style type="text/css">
