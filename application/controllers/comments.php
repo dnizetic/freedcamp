b7d2124398fb40abcd002650c302d21fd1da69a8
dnizetic <denis_nizetic@hotmail.com>
2017-01-29 17:58:17 +0100

    Fallback to 0000 date if no comments in table.

--- application/controllers/comments.php
+++ application/controllers/comments.php
@@ -9,7 +9,7 @@ class Comments extends MY_Controller {
 
         $this->load->model('comments_model');
 
-        
+
         /**
          * Sort comments by date before fetching them.
          */
@@ -17,22 +17,32 @@ class Comments extends MY_Controller {
             'item_id' => $item_id
         ));
 
-        
+
         /**
          * Long polling needs to pass item id in order
          * to check for new rows.
          */
         $data['item_id'] = $item_id;
-        
-        
+
+
         /**
          * Get date field from last row. With this date we'll check for newly added rows.
          * If a row in DB has a greater add date than this, then we'll know
          * it's a newly added row.
          */
-        $last_row_index = count($data['comments']) - 1;
-        $data['latest_date'] = $data['comments'][$last_row_index]->date;
-        
+        if (count($data['comments']) > 0) {
+            $last_row_index = count($data['comments']) - 1;
+            $data['latest_date'] = $data['comments'][$last_row_index]->date;
+        } else {
+            /**
+             * Fallback gracefully. This will still make
+             * the functionalities work, even if there's no rows
+             * in the table.
+             */
+            $data['latest_date'] = '0000-00-00';
+        }
+
+
 
         $this->call_template('comments/show', $data);
     }
