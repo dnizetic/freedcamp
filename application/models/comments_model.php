d738ecd72e8f81e302e2aa4c14457875bb6fbbf5
dnizetic <denis_nizetic@hotmail.com>
2017-01-29 17:11:00 +0100

    Setup model function to check for new comments

--- application/models/comments_model.php
+++ application/models/comments_model.php
@@ -2,9 +2,24 @@
 
 class Comments_model extends MY_Model {
 
-   
     protected $_table = 'comments';
     protected $primary_key = 'commentid';
+
+    /**
+     * Returns all comments entered after $latest_date.
+     * @param type $current_num
+     * @param type $item_id
+     * @return type
+     */
+    public function get_new_comments($latest_date, $item_id) {
+
+        $comments = $this->order_by('date')->get_many_by(array(
+            'item_id' => $item_id,
+            'date >' => $latest_date
+        ));
+
+        return $comments;
+    }
 
 }
 
