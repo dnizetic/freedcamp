2d96d9bdbc4acf4e438d80ed5c3adf66af5ae369
dnizetic <denis_nizetic@hotmail.com>
2017-01-29 15:04:16 +0100

    Setup Base Model, Base Controller, and Template engine

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+class MY_Controller extends CI_Controller {
+
+
+    function __construct() {
+        parent::__construct();
+        $libs = array(
+            'template',
+        );
+        $this->load->library($libs);
+    }
+
+    protected function call_template($view_path, $data) {
+        $this->template->write_view('content', $view_path, $data);
+        $this->template->render();
+    }
+
+    /**
+     * Standard JSON response
+     * @param type $status
+     * @param type $message 
+     */
+    function json($status, $message, $code = 200) {
+        $this->output
+                ->set_content_type('application/json')
+                ->set_status_header($code)
+                ->set_output(json_encode(array(
+                    'status' => $status,
+                    'message' => $message
+        )));
+    }
+
+}
